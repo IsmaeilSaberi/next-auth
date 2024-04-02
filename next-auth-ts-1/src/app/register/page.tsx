0ba72470b0c080d5679bd3cff0dda9deb09e5993
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-04-02 11:08:06 -0700

    I complete the ui and apis for register to mongo db!

--- next-auth-ts-1/src/app/register/page.tsx
+++ next-auth-ts-1/src/app/register/page.tsx
@@ -1,12 +1,62 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    if (!isValidEmail(email)) {
+      setError("Email is invalid! ");
+      return;
+    }
+
+    if (!password || password.length < 8) {
+      setError("Password is invalid! ");
+      return;
+    }
+
+    try {
+      const res = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      if (res.status == 400) {
+        setError("this email is alredy registered!");
+      }
+      if (res.status == 200) {
+        setError("");
+        router.push("/login");
+      }
+    } catch (error) {
+      setError("Error, try again");
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col justify-between items-center p-24">
       <div className="bg-[#212121] p-8 rounded shadow-md w-96">
         <h1 className="text-4xl text-center font-semibold mb-8">Register</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             className="w-full border-gray-300 text-black rounded px-3 py-2 mb-4 focus:border-blue-400 focus:text-black"
@@ -25,6 +75,7 @@ const Register = () => {
           >
             Register
           </button>
+          <p className="text-red-600 text-[16px] mb-4">{error && error}</p>
         </form>
         <div className="text-center text-gray-500 mt-4">-- OR --</div>
         <Link
